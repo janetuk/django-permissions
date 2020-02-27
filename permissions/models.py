faf4676c7a93965e3b564a871442bfbdc1521a10
john-ramsden <john.ramsden@jisc.ac.uk>
2020-02-27 12:02:01 +0000

    Fix User model in models.py

--- permissions/models.py
+++ permissions/models.py
@@ -1,6 +1,9 @@
-# django imports
+
 from django.db import models
-from django.contrib.auth.models import User
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 from django.contrib.auth.models import Group
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
